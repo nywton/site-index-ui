a9afcd1afd6e57c6aa2dafdac7a21ccb3d701aaf
Nywton Barros <nywton.barros@gmail.com>
2019-04-07 15:33:11 -0300

    Fix

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.a817a49293777dd17079b622a2e2ded5.js"
+  "/build/precache-manifest.01e15bec30cfa552af95e679f41af472.js"
 );
 
 workbox.clientsClaim();
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -l
 
+yarn build
+git add .
+git commit -a -m "Fix"
 git push heroku master
 git push origin master
 firebase deploy
