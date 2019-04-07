26a8879c47509ebdbcb85e6b64de4be89b829fa8
Nywton Barros <nywton.barros@gmail.com>
2019-04-07 14:07:57 -0300

    Firebase config

--- src/base.js
+++ src/base.js
@@ -12,6 +12,5 @@ const config = {
 
 const app = firebase.initializeApp(config)
 const base = Rebase.createClass(app.database())
-const facebookProvider = new firebase.auth.FacebookAuthProvider()
 
-export { app, base, facebookProvider }
+export { app, base }
