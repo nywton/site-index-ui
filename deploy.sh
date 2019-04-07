d56a785ac18a635b9c11326c4ea804ddb5609d5b
Nywton Barros <nywton.barros@gmail.com>
2019-04-07 16:18:50 -0300

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -l
 
-yarn build
+rm -rm node_modules/
+rm -rf build/
+yarn install
+yarn build production
 git add .
-git commit -a -m "Fix"
+git commit -a -m "Deploy"
 git push heroku master
 git push origin master
 
