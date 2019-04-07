17a114ab334109d6a4360766acc9201824dfaa3b
Nywton Barros <nywton.barros@gmail.com>
2019-04-07 14:07:08 -0300

    Firebase config

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import 'flat-ui/css/flat-ui.css';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as firebase from 'firebase';
+import registerServiceWorker from './registerServiceWorker';
 
 
 
@@ -14,4 +15,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+registerServiceWorker();
